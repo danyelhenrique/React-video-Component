e87754fada74ede92646586acbd4c1205f7cc1e2
danyelhenrique <danyelhenriquefidel@gmail.com>
2019-08-29 00:08:43 -0300

    make renderVideo Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,82 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
+import video from "./videos/video.mp4";
 
 import "./App.css";
 
 import HEADER from "./components/header";
-import VIDEOCOMPONENT from "./components/videoComponent";
+import RENDERVIDEO from "./components/videoComponent";
 
 function App() {
   return (
     <div className="App">
       <HEADER></HEADER>
-      <VIDEOCOMPONENT></VIDEOCOMPONENT>
+      <RENDERVIDEO></RENDERVIDEO>
       {/* <h1>ola</h1> */}
+      {/* <TESTE></TESTE> */}
     </div>
   );
 }
+
+// function TESTE() {
+//   useEffect(() => {
+//     setInterval(() => {
+//       progressBarvideo();
+//     }, 1000);
+//   }, []);
+
+//   const [progressBar, SetprogressBar] = useState({ progress: 0 });
+//   const rf = useRef();
+
+//   function progressBarvideo() {
+//     const video = rf.current;
+//     const totalProgress = parseInt((video.currentTime / video.duration) * 100);
+//     SetprogressBar({ progress: totalProgress });
+//   }
+
+//   function setTime(e) {
+//     const video = rf.current;
+//     const {
+//       current: { duration, clientWidth }
+//     } = rf;
+//     const timeUpdate = duration * (e.nativeEvent.offsetX / clientWidth);
+//     video.currentTime = timeUpdate;
+//   }
+
+//   function setVolume(e) {
+//     const video = rf.current;
+//     video.volume = e.target.value / 100;
+//   }
+
+//   const divSt = {
+//     backgroundColor: "#00B1E1",
+//     width: "100%",
+//     height: "100px"
+//   };
+//   const divStitem = {
+//     backgroundColor: "#27ae60",
+//     width: `${progressBar.progress}%`,
+//     height: "100px"
+//   };
+//   return (
+//     <div className="algo">
+//       <video controls className="video-item" ref={rf}>
+//         <source src={video} type="video/mp4" />
+//       </video>
+//       <div class="progress" style={divSt} onClick={e => setTime(e)}></div>
+//       <div class="progress-item" style={divStitem}>
+//         {`${progressBar.progress}%`}
+//       </div>
+//       <input
+//         className="volume"
+//         type="range"
+//         min="0"
+//         max="100"
+//         // value={rf.current.volume}
+//         step="1"
+//         onChange={e => setVolume(e)}
+//       />
+//     </div>
+//   );
+// }
 
 export default App;
--- src/components/videoComponent/index.jsx
+++ src/components/videoComponent/index.jsx
@@ -1,64 +1,80 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./index.css";
 import video from "../../videos/video.mp4";
 
-import { FiVolume1 } from "react-icons/fi"; // volume 1
 import { FiSkipForward } from "react-icons/fi"; //next
 import { FiSkipBack } from "react-icons/fi"; // prev
+//
+//
+import { FiVolume1 } from "react-icons/fi"; // volume 1
 import { FiSettings } from "react-icons/fi"; // settings
-import { FiPlay } from "react-icons/fi"; // play
-import { FiPause } from "react-icons/fi"; // pause
-import { FiMaximize } from "react-icons/fi"; // full screen
+import { FiPlay } from "react-icons/fi"; // play //ok
+import { FiPause } from "react-icons/fi"; // pause // ok
+import { FiMaximize } from "react-icons/fi"; // full screen //ok
 import { FiMinimize } from "react-icons/fi"; // minimazi screen
 
 import { FaRegClosedCaptioning } from "react-icons/fa"; // captions
 
-export default function INDEX() {
-  const ref = useRef();
-  const [duration, setDuration] = useState({ value: 0 });
+function RENDERVIDEO() {
   useEffect(() => {
     setInterval(() => {
-      setDuration({ value: ref.current.currentTime });
+      progressBarvideo();
     }, 1000);
   }, []);
 
-  function play() {
-    return () => ref.current.play();
+  const [progressBar, SetprogressBar] = useState({ progress: 0 });
+  const rf = useRef();
+
+  function progressBarvideo() {
+    const video = rf.current;
+    const totalProgress = parseInt((video.currentTime / video.duration) * 100);
+    SetprogressBar({ progress: totalProgress });
   }
 
-  function pause() {
-    return () => ref.current.pause();
+  function setTime(e) {
+    const video = rf.current;
+    const {
+      current: { duration, clientWidth }
+    } = rf;
+    const timeUpdate = duration * (e.nativeEvent.offsetX / clientWidth);
+    video.currentTime = timeUpdate;
   }
 
-  function fullScreen() {
-    return () =>
-      ref.current.requestFullscreen()
-        ? ref.current.fullscreenElement
-        : ref.current.exitFullscreen();
+  function setVolume(e) {
+    const video = rf.current;
+    video.volume = e.target.value / 100;
   }
 
+  const divSt = {
+    backgroundColor: "#00B1E1",
+    width: "100%",
+    height: "100px"
+  };
+  const divStitem = {
+    backgroundColor: "#27ae60",
+    width: `${progressBar.progress}%`,
+    height: "100px"
+  };
   return (
-    <div className="video-component">
-      <video controls className="video-item" ref={ref}>
+    <div className="algo">
+      <video controls className="video-item" ref={rf}>
         <source src={video} type="video/mp4" />
       </video>
-      <div className="controls">
-        <button onClick={play()}>
-          <FiPlay></FiPlay>
-        </button>
-        <button onClick={pause()}>
-          <FiPause></FiPause>
-        </button>
-        <button onClick={fullScreen()}>
-          <FiMaximize></FiMaximize>
-        </button>
-        <div
-          className="current"
-          //   style={{ width: `${ref.current.currentTime}px` }}
-        >
-          {duration.value}
-        </div>
+      <div class="progress" style={divSt} onClick={e => setTime(e)}></div>
+      <div class="progress-item" style={divStitem}>
+        {`${progressBar.progress}%`}
       </div>
+      <input
+        className="volume"
+        type="range"
+        min="0"
+        max="100"
+        // value={rf.current.volume}
+        step="1"
+        onChange={e => setVolume(e)}
+      />
     </div>
   );
 }
+
+export default RENDERVIDEO;
